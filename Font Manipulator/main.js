bbd977d435d0d5f39de7f8e77f8bb9d83c9ac1c0
Abhimanyu Mukherjee <75936349+arjo2009@users.noreply.github.com>
2021-05-28 20:45:14 +0530

    Add files via upload

---
+++ Font Manipulator/main.js
@@ -0,0 +1,43 @@
+txt = "";
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 600);
+    video.position(100, 200);
+
+    canvas = createCanvas(650, 500);
+    canvas.position(850, 250);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("Model is Loaded!")
+}
+function setText(){
+    txt = document.getElementById("txt").value;
+    window.alert(txt);
+}
+function gotPoses(results){
+    if(results.length > 0){
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X = " + noseX + " Nose Y = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("Left Wrist X = " + leftWristX + " Right Wrist X = " + rightWristX + " Difference = " + difference);
+    }
+}
+function draw(){
+    background("#8e07f5");
+    fill("#ffffff");
+    stroke("#000000");
+    text(txt, noseX, noseY);
+    textSize(difference);
+}
