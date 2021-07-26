a9604e28af29e14cd3108d5dc69b71420b19e3ed
Abhimanyu Mukherjee <75936349+arjo2009@users.noreply.github.com>
2021-07-26 20:02:08 +0530

    Add files via upload

---
+++ Let's Chat/Login/main.js
@@ -0,0 +1,9 @@
+
+
+function signIn(){
+        var username = document.getElementById("username").value;
+        var email = document.getElementById("email").value;
+        localStorage.setItem("Username",username);
+        localStorage.setItem("Email",email);
+        window.location="homepage.html";
+}
