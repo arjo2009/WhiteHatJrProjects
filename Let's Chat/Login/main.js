f9cc5c602dff7264e373963d21425fa1f20e4883
Abhimanyu Mukherjee <75936349+arjo2009@users.noreply.github.com>
2021-07-26 20:07:56 +0530

    Update main.js

--- Let's Chat/Login/main.js
+++ Let's Chat/Login/main.js
@@ -5,5 +5,4 @@ function signIn(){
         var email = document.getElementById("email").value;
         localStorage.setItem("Username",username);
         localStorage.setItem("Email",email);
-        window.location="https://arjo2009.github.io/WhiteHatJrProjects/Let's%20Chat/Homepage/";
 }
