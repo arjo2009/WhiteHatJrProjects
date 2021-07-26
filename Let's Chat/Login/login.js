59dfc1017f57670a684d5ada177912d2927666a7
Abhimanyu Mukherjee <75936349+arjo2009@users.noreply.github.com>
2021-07-26 20:01:51 +0530

    Delete login.js

--- Let's Chat/Login/login.js
+++
@@ -1,9 +0,0 @@
-
-
-function signIn(){
-        var username = document.getElementById("username").value;
-        var email = document.getElementById("email").value;
-        localStorage.setItem("Username",username);
-        localStorage.setItem("Email",email);
-        window.location="homepage.html";
-}
